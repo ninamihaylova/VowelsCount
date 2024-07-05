bbf3d7fbe40ac28fb26289077422960bfce31cd2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 20:02:46 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿string input = Console.ReadLine();
+
+PrintVowelsCounterInText(input);
+
+static void PrintVowelsCounterInText(string text)
+{
+    int counter = 0;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        char currentLetter = text[i];
+
+        switch (currentLetter)
+        {
+            case 'a': counter++; break;
+            case 'o': counter++; break;
+            case 'u': counter++; break;
+            case 'i': counter++; break;
+            case 'e': counter++; break;
+            case 'A': counter++; break;
+            case 'O': counter++; break;
+            case 'U': counter++; break;
+            case 'I': counter++; break;
+            case 'E': counter++; break;
+        }
+    }
+    Console.WriteLine(counter);
+    
+}
